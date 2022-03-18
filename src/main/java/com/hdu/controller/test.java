b03fcca7a245cb5a50d7fbd129e999ab3c14ca4c
luca <xwjhdu@163.com>
2022-03-19 02:07:57 +0800

    first commit

---
+++ src/main/java/com/hdu/controller/test.java
@@ -0,0 +1,12 @@
+package com.hdu.controller;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+
+
