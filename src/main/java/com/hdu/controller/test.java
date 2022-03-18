5f6dbb126223456e544674d772689e836431433a
luca <xwjhdu@163.com>
2022-03-19 02:13:20 +0800

    secound commit

--- src/main/java/com/hdu/controller/test.java
+++ src/main/java/com/hdu/controller/test.java
@@ -5,6 +5,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world1");
     }
 }
 
